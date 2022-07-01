469a74586e1d2e9971b92f6c0b87ee58fc77ffcd
BirlaAishwarya <aishwarya.birla@thoughtworks.com>
2022-07-01 22:17:24 +0530

    alter name column to full_name in users table

---
+++ src/main/resources/db/migration/V1_1__CreateUsersTable.sql
@@ -0,0 +1,5 @@
+CREATE table Users(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    address VARCHAR(60) NOT NULL
+);
---
+++ src/main/resources/db/migration/V1_2__CreateProductsTable.sql
@@ -0,0 +1,5 @@
+CREATE table Products(
+    id INT PRIMARY KEY,
+    name VARCHAR(20),
+    price INT
+);
